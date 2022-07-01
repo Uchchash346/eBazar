1c680eecbed3088336938d86d7401026ab84a011
Uchchash346 <mru416@gmail.com>
2022-07-01 12:49:13 +0600

    Navbar setup

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import Head from 'next/head';
+import Navbar from './Navbar';
+import Footer from './Footer';
 
-const Layout = () => {
+const Layout = ({ children }) => {
     return (
         <div>
-            
+            <Head>
+                <title>eBazar Store</title>
+            </Head>
+            <header>
+                <Navbar />
+            </header>
+            <main className="main-container">
+                {children}
+            </main>
+            <footer>
+                <Footer />
+            </footer>
         </div>
     );
 };
