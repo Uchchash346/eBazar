ae93520bc5cb191b50f1ede5dcb2d3d40e0da428
Uchchash346 <mru416@gmail.com>
2022-07-02 08:46:21 +0600

    Set success page

---
+++ pages/api/stripe.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe(
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+);
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        try {
+            const params = {
+                submit_type: 'pay',
+                mode: 'payment',
+                payment_method_types: ['card'],
+                billing_address_collection: 'auto',
+                shipping_options: [
+                    { shipping_rate: 'shr_1LGvIzLWA4NMH0y03B9I7eGe' },
+                ],
+                line_items: req.body.map((item) => {
+                    const img = item.image[0].asset._ref;
+                    const newImage = img.replace('image-', 'https://cdn.sanity.io/images/uviuemhz/production/').replace('-webp', '.webp');
+
+                    return {
+                        price_data: {
+                            currency: 'usd',
+                            product_data: {
+                                name: item.name,
+                                images: [newImage],
+                            },
+                            unit_amount: item.price * 100,
+                        },
+                        adjustable_quantity: {
+                            enabled: true,
+                            minimum: 1,
+                        },
+                        tax_behavior: "exclusive",
+                        quantity: item.quantity
+                    }
+                }),
+                success_url: `${req.headers.origin}/success`,
+                cancel_url: `${req.headers.origin}/canceled`,
+            }
+
+            // Create Checkout Sessions from body params.
+            const session = await stripe.checkout.sessions.create(params);
+
+            res.status(200).json(session);
+        } catch (err) {
+            res.status(err.statusCode || 500).json(err.message);
+        }
+    } else {
+        res.setHeader('Allow', 'POST');
+        res.status(405).end('Method Not Allowed');
+    }
+}
